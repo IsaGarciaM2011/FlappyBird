2f4e48424bf65c838550b43a8fb7450968be51aa
fdsfvrsdg <isagarciam012@gmail.com>
2023-06-16 18:54:01 -0500

    first commit

---
+++ js/game.js
@@ -0,0 +1,31 @@
+let contexto = document.getElementById("gameCanvas").getContext("2d")
+contexto.canvas.width = 300
+contexto.canvas.height = 700
+
+let fps = 60
+let gravedad = 1.5
+
+let bird = {
+    x: 100,
+    y: 150,
+    w:50,
+    h:50
+}
+
+setInterval(loop, 1000 / fps )
+
+function loop(){
+    contexto.clearRect(0, 0, 300, 700)
+    contexto.fillStyle = "rgba(100, 0, 0, 1)"
+    contexto.fillRect(bird.x, bird.y, bird.w, bird.h)
+
+    bird.y += gravedad 
+}
+
+// controles
+
+function press(){
+    bird.y -= 25
+}
+
+window.addEventListener("keyup", press)
